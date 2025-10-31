7c67cce08b18c7992090a8beae0db183abe8d48b
Arbinda Yadav <arbind91117@gmail.com>
2025-10-31 07:02:43 +0545

    Update api.jsx

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://cloud-backends.onrender.com/api";
 
 let accessToken = localStorage.getItem("accessToken");
 let refreshToken = localStorage.getItem("refreshToken");
