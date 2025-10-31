627d154e55d44677ed0be519fd3de07ffe7d2412
Arbinda Yadav <arbind91117@gmail.com>
2025-10-31 06:58:22 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL (Vite default)
+  origin: "https://cloud-frontend-lvgy.onrender.com", // frontend URL (Vite default)
   credentials: true
 }));
 app.use(express.json());
